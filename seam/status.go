726fb93b3bb77ab485e92b9d2abd1a40d94f0b2b
Austin Massey <austinmassey.am@gmail.com>
2024-11-19 06:12:23 -0600

    added timeout to NewClient

--- seam/status.go
+++ seam/status.go
@@ -1,10 +1,13 @@
 package seam
 
 import (
+	"net/http"
+	"time"
+
 	goclient "github.com/seamapi/go/client"
 )
 
 func Authenticate(apiKey string) *goclient.Client {
-	client := goclient.NewClient(goclient.WithApiKey(apiKey))
+	client := goclient.NewClient(goclient.WithApiKey(apiKey), goclient.WithHTTPClient(&http.Client{Timeout: 10 * time.Second}))
 	return client
 }
